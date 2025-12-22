0d2baffc914a7e030de551a1ff12383eb989ccb8
KaykyOne <kaykyzioti@gmail.com>
2025-12-22 17:30:14 -0300

    att calculadora 1

--- src/components/calculator.tsx
+++ src/components/calculator.tsx
@@ -1,18 +1,59 @@
+import { set } from 'astro:schema';
 import React, { useState, useEffect } from 'react'
 
 type tipos = 'carroMoto' | 'moto' | 'carro'
-const valorAula = 100
+const valorOriginalAula = 100
 const custoAluguelCarro = 49.99;
+
+const taxas: any = {
+    1: 3.51,
+    2: 4.36,
+    3: 5.15,
+    4: 5.93,
+    5: 6.72,
+    6: 7.51,
+    7: 8.86,
+    8: 9.65,
+    9: 10.43,
+    10: 11.22,
+    11: 12.00,
+    12: 12.78,
+    13: 13.57,
+    14: 14.35,
+    15: 15.14,
+    16: 15.92,
+    17: 16.70,
+    18: 17.49
+};
 
 export default function Calculator() {
     const [numAulasMoto, setNumAulasMoto] = useState<number>(2)
     const [numAulasCarro, setNumAulasCarro] = useState<number>(2)
     const [tipo, setTipo] = useState<tipos>('carroMoto')
+    const [valorTotal, setValorTotal] = useState<number>(0)
+    const [valorTotalSemJuros, setValorTotalSemJuros] = useState<number>(0)
+    const [valorPorAula, setValorPorAula] = useState<number>(0)
+    const [valorAluguel, setValorAluguel] = useState<number>(0)
+    const [valorAula, setValorAula] = useState<number>(100)
+    const [numeroParcelas, setNumeroParcelas] = useState<number>(1)
 
     useEffect(() => {
         setNumAulasMoto(2)
         setNumAulasCarro(2)
     }, [tipo])
+
+    useEffect(() => {
+        calcularValorComDesconto()
+    }, [numAulasMoto, numAulasCarro, tipo])
+
+    useEffect(() => {
+        if (numeroParcelas > 6) {
+            const resultado = calcularValorComJuros(valorTotalSemJuros, numeroParcelas)
+            setValorTotal(resultado)
+        } else {
+            setValorTotal(valorTotalSemJuros)
+        }
+    }, [numeroParcelas, valorTotalSemJuros])
 
     const formatter = new Intl.NumberFormat('pt-BR', {
         style: 'currency',
@@ -27,6 +68,13 @@ export default function Calculator() {
         } else if (tip === 'carro') {
             setNumAulasCarro(valor)
         }
+    }
+
+    function calcularValorComJuros(valor: number, parcelas: number) {
+        // const TAXA_BASE = 0.0351;        // 3,51%
+        // const JUROS_MENSAL = 0.018;      // 1,8%
+
+        return taxas[parcelas] ? valor * (1 + taxas[parcelas] / 100) : valor;
     }
 
     const RenderInputs = () => {
@@ -80,12 +128,44 @@ export default function Calculator() {
         return valor - (valor * (desconto / 100))
     }
 
-    const calcularValorComDesconto = (valor: number) => {
-        let valorDesconto = 0;
-        if (numAulasCarro + numAulasMoto >= 10) {
-            return valor - calcularCustoAluguel() - 0.02;
+    const calcularValorComDesconto = () => {
+        let aulasTotais = 0;
+        let valorAluguel = calcularCustoAluguel();
+        let valorAulaLocal = 100;
+
+        if (tipo) {
+            if (tipo === 'carro') {
+                aulasTotais = numAulasCarro;
+            }
+
+            if (tipo === 'moto') {
+                aulasTotais = numAulasMoto;
+            }
+
+            if (tipo === 'carroMoto') {
+                aulasTotais = numAulasCarro + numAulasMoto;
+            }
         }
-        return valor + calcularCustoAluguel();
+        if (aulasTotais >= 10 && aulasTotais < 15) {
+            valorAulaLocal = 90;
+        }
+        else if (aulasTotais >= 15 && aulasTotais < 20) {
+            valorAulaLocal = 85;
+            valorAluguel = valorAluguel / 2 - 0.01;
+        }
+        else if (aulasTotais >= 20) {
+            valorAulaLocal = 80;
+            valorAluguel = 0;
+        } else {
+            valorAulaLocal = 100;
+            valorAluguel = calcularCustoAluguel();
+        }
+
+        const valorT = ((aulasTotais * valorAulaLocal) + valorAluguel)
+        setValorAula(valorAulaLocal);
+        setValorTotalSemJuros(valorT);
+        setValorPorAula(aulasTotais > 0 ? (valorT - valorAluguel) / aulasTotais : 0);
+        setValorAluguel(valorAluguel);
     }
 
     const calcularCustoAluguel = () => {
@@ -95,10 +175,39 @@ export default function Calculator() {
         return custoAluguelCarro * 2 + 0.01
     }
 
-    const valorTotal = calcularValorComDesconto((numAulasMoto * valorAula) + (numAulasCarro * valorAula));
+
+    const testDescontoAula = valorOriginalAula - valorAula > 0 ? true : false;
+    const testDescontoAluguel = calcularCustoAluguel() - valorAluguel;
+    const descontoAluguel = testDescontoAluguel * 100 / calcularCustoAluguel();
+    const descontoAula = (valorOriginalAula - valorAula) * 100 / valorOriginalAula;
+
+
+    const DivDescontoAula = () => {
+        return (
+            <div className='bg-amber-600 flex flex-col p-3 rounded-lg mt-5'>
+                <h1>Você recebeu um desconto de <strong>{descontoAula.toFixed(0)}%</strong> nas aulas!</h1>
+            </div>
+        )
+    }
+
+    const DivDescontoVeiculo = () => {
+        return (
+            <div className='bg-green-600 flex flex-col p-3 rounded-lg mt-5'>
+                <h1>Você recebeu um desconto de <strong>{descontoAluguel.toFixed(0)}%</strong> no aluguel dos veículos!</h1>
+            </div>
+        )
+    }
+
+    const reset = () => {
+        setNumAulasMoto(2)
+        setNumAulasCarro(2)
+        setValorAula(100)
+        setTipo('carroMoto')
+        setNumeroParcelas(1)
+    }
 
     return (
-        <div className='w-full bg-linear-65 from-primary/80 to-primary text-white p-5 rounded-lg shadow-lg grid grid-cols-1 md:grid-cols-2 gap-4 mb-10'>
+        <div className='w-full bg-linear-65 p-8 from-primary/80 to-primary text-white rounded-3xl shadow-lg grid grid-cols-1 lg:grid-cols-2 gap-4 mb-10 transition-all duration-300 hover:shadow-2xl hover:-translate-y-1'>
             <div className='flex flex-col'>
 
                 <h3 className='text-md uppercase opacity-70'>Descubra qual o pacote</h3>
@@ -142,11 +251,35 @@ export default function Calculator() {
                     </div>
                 </fieldset>
                 <RenderInputs />
-
+                <div className='mt-3 gap-4 flex flex-col'>
+                    <label htmlFor='numeroParcelas' className='mt-6 text-lg font-medium'>Número de parcelas: {numeroParcelas}</label>
+                    <input type="range" name="numeroParcelas" value={numeroParcelas} min={1} max={18} onChange={(e) => setNumeroParcelas(Number(e.target.value))} />
+                </div>
+                <div className='grid-cols-1 lg:grid-cols-2 gap-2 mt-5 hidden lg:grid'>
+                    <button className='bg-secondary py-3 px-6 text-white w-full rounded-lg cursor-pointer hover:opacity-90 transition-opacity' onClick={() => reset()}>
+                        Reiniciar
+                    </button>
+                    <button className='bg-green-700 py-3 px-6 text-white w-full rounded-lg cursor-pointer hover:opacity-90 transition-opacity'>
+                        Quero iniciar agora!
+                    </button>
+                </div>
             </div>
             <div className='flex flex-col ga-1'>
                 <h3 className='text-xl'>Valor total aproximado:</h3>
                 <h1 className='text-6xl font-semibold'>{formatter.format(valorTotal)}</h1>
+                {numeroParcelas > 1 && <p className='mt-3 opacity-00 font-light'>ou em <strong className='uppercase font-semibold text-2xl'>{numeroParcelas}x</strong> de <strong className='uppercase font-semibold text-2xl'>{formatter.format(valorTotal / numeroParcelas)}</strong> {valorTotal == valorTotalSemJuros ? 'SEM JUROS' : 'com juros da maquininha'}</p>}
+                <h3 className='text-xl mt-10'>Valor por aula aproximado:</h3>
+                <h1 className='text-4xl font-semibold'>{formatter.format(valorPorAula)}</h1>
+                {testDescontoAula && <DivDescontoAula />}
+                {(testDescontoAluguel > 0) && <DivDescontoVeiculo />}
+                <div className='grid grid-cols-1 gap-2 mt-5  lg:hidden'>
+                    <button className='bg-secondary py-3 px-6 text-white w-full rounded-lg cursor-pointer hover:opacity-90 transition-opacity' onClick={() => reset()}>
+                        Reiniciar
+                    </button>
+                    <button className='bg-green-700 py-3 px-6 text-white w-full rounded-lg cursor-pointer hover:opacity-90 transition-opacity'>
+                        Quero iniciar agora!
+                    </button>
+                </div>
             </div>
         </div>
     )
